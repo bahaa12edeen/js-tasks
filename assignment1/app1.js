b1738e8c7e8b6af5d023653bfc0e1f85af6cdede
unknown <baldee229@gmail.com>
2022-03-20 19:43:30 +0300

    first time - js

--- assignment1/app1.js
+++ assignment1/app1.js
@@ -3,11 +3,11 @@ y = window.prompt("enter y val: ");
 
 if(x > y)
 {
-console.log("Hello World");
+console.log("Hello World" + "\n" + "x: " + x + "\n" + "y: " + y);
 document.write("Hello World");
 window.alert("Hello World");
 }else{
-console.log("Goodbye");
+console.log("Goodbye" + "\n" + "x: " + x + "\n" + "y: " + y);
 document.write("Goodbye");
 window.alert("Goodbye");
 }
--- assignment1/app2.js
+++ assignment1/app2.js
@@ -4,41 +4,41 @@ let day = window.prompt("enter value from 1 - 7 to choose day a week: ");
 
 switch(day){
     case 1:
-        console.log("your day is: Saturday");
+        console.log("your day is: Saturday" + "\n" + "day: " + day);
         document.write("your day is: Saturday");
     break;
 
     case 2:
-        console.log("your day is: Sunday");
+        console.log("your day is: Sunday" + "\n" + "day: " + day);
         document.write("your day is: Sunday");
     break;
 
     case 3:
-        console.log("your day is: Monday");
+        console.log("your day is: Monday" + "\n" + "day: " + day);
         document.write("your day is: Monday");
     break;
 
     case 4:
-        console.log("your day is: Tuesday");
+        console.log("your day is: Tuesday" + "\n" + "day: " + day);
         document.write("your day is: Tuesday");
     break;
 
     case 5:
-        console.log("your day is: Wednesday");
+        console.log("your day is: Wednesday" + "\n" + "day: " + day);
         document.write("your day is: Wednesday");
     break;
 
     case 6:
-        console.log("your day is: Thursday");
+        console.log("your day is: Thursday" + "\n" + "day: " + day);
         document.write("your day is: Thursday");
     break;
 
     case 7:
-        console.log("your day is: Friday");
+        console.log("your day is: Friday" + "\n" + "day: " + day);
         document.write("your day is: Friday");
     break;
 
     default:
-        console.log("value entered not valid");
-        document.write("value entered not valid");
+        console.log("value entered not valid" + "\n" + "day: " + day);
+        document.write("value entered not valid" + "<br>" + "- day: " + day);
 }

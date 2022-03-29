eb2a47f0715455d2d6e77d4c1ff7216687be59cc
unknown <baldee229@gmail.com>
2022-03-29 23:01:37 +0300

    assignmrnt wit API

--- assignment5/app1.js
+++ assignment5/app1.js
@@ -1,34 +1,40 @@
-let tr,
+let loca,
+    tr,
     td1,
     td2,
     td3,
     td4,
-    m_arr=[],
-    arr=[];
-    let lcl=[];
+    td5,
+    td6,
+    td7,
+    image,
+    arr=[],
+    lcl=[];
+
     window.localStorage.i;
     if(window.localStorage.i == undefined){
         window.localStorage.i = 0;
     }
-
     let i = window.localStorage.i;
 
 document.getElementById("submit").onclick = function(){
     i = window.localStorage.i;
 
     if(!(document.getElementById("name").value == "" || document.getElementById("name").value == " ")){
+        arr[i] = [];
+
         tr = document.createElement("tr");
         td1 = document.createElement("td");
         td1.textContent = document.getElementById("name").value;
-        arr[0] = td1.textContent;
+        arr[i][0] = td1.textContent;
 
         td2 = document.createElement("td");
         td2.textContent = document.getElementById("mobile").value;
-        arr[1] = td2.textContent;
+        arr[i][1] = td2.textContent;
 
         td3 = document.createElement("td");
         td3.textContent = (Math.floor(Math.random() * (500 - 100) ) + 98) + "$";
-        arr[2] = td3.textContent;
+        arr[i][2] = td3.textContent;
 
         td4 = document.createElement("td");
         if(parseInt(td3.textContent) > 100){
@@ -36,66 +42,104 @@ document.getElementById("submit").onclick = function(){
         }else{
             td4.textContent = "used";
         }
-        arr[3] = td4.textContent;
+        arr[i][3] = td4.textContent;
 
-        m_arr[i] = arr;
+        td5 = document.createElement("td");
 
-         
+        td6 = document.createElement("td");
 
-        window.localStorage.m_arr =  JSON.stringify(m_arr);
+        td7 = document.createElement("td");
+
+        image = document.createElement("img");
+
+        loca = document.getElementById("loc").value;
+
+        async function get(){
+            const response = await fetch(`https://eu1.locationiq.com/v1/search.php?key=pk.540d315694ad18fc6d9a33cfc4fab719&q=${loca}&format=json`);
+            const data = await response.json();
+
+            image.src = data[0].icon;
+            td6.textContent = data[0].lat;
+            td7.textContent = data[0].lon;
+
+            arr[i][4] = image.src;
+            arr[i][5] = td6.textContent;
+            arr[i][6] = td7.textContent;
+        }
+
+        get();
+
+        setTimeout(function(){
+            localStorage.setItem("m_arr", JSON.stringify(arr));
+
+            console.log("i: "+i);
+            console.log("arr: "+arr);
+            console.log(localStorage.m_arr);
+        }, 1000);
+
+
         
     
         document.getElementById("table").appendChild(tr);
         tr.appendChild(td1);
         tr.appendChild(td2);
         tr.appendChild(td3);
-        tr.appendChild(td4);    
-        
-        console.log("i: "+i);
-        console.log("m_arr: "+m_arr);
+        tr.appendChild(td4);   
+        tr.appendChild(td5);
+        tr.appendChild(td6);
+        tr.appendChild(td7);    
+        td5.appendChild(image);
 
         window.localStorage.i++;
     }
 
-    // console.log();
-    lcl = JSON.parse(window.localStorage.m_arr);
+    lcl = JSON.parse(localStorage.getItem("m_arr"));
 }
-// window.localStorage.m_arr =  JSON.stringify(m_arr);
-lcl = JSON.parse(window.localStorage.m_arr);
-console.log("lcl: "+lcl);
+
+if(window.localStorage.m_arr != undefined){
+    lcl = JSON.parse(localStorage.getItem("m_arr"));
+    console.log("lcl: "+lcl);
+    renderOrder();
+}
 
 function renderOrder(){
     for(let i=0; i < lcl.length; i++){
         tr = document.createElement("tr");
         td1 = document.createElement("td");
-        td1.textContent = document.getElementById("name").value;
+        td1.textContent = lcl[i][0];
 
         td2 = document.createElement("td");
-        td2.textContent = document.getElementById("mobile").value;
+        td2.textContent = lcl[i][1];
 
         td3 = document.createElement("td");
-        td3.textContent = (Math.floor(Math.random() * (500 - 100) ) + 98) + "$";
+        td3.textContent = lcl[i][2];
 
         td4 = document.createElement("td");
-        if(parseInt(td3.textContent) > 100){
-            td4.textContent = "new device";
-        }else{
-            td4.textContent = "used";
-        }
+        td4.textContent = lcl[i][3];
+
+        td5 = document.createElement("td");
+        image = document.createElement("img");
+        image.src = lcl[i][4];
+
+        td6 = document.createElement("td");
+        td6.textContent = lcl[i][5];
+
+        td7 = document.createElement("td");
+        td7.textContent = lcl[i][6];
+
+
 
         document.getElementById("table").appendChild(tr);
         tr.appendChild(td1);
         tr.appendChild(td2);
         tr.appendChild(td3);
-        tr.appendChild(td4);    
-
-        td1.textContent = lcl[i][0];
-        td2.textContent = lcl[i][1];
-        td3.textContent = lcl[i][2];
-        td4.textContent = lcl[i][3];        
+        tr.appendChild(td4);
+        tr.appendChild(td5);
+        tr.appendChild(td6);
+        tr.appendChild(td7);
+        td5.appendChild(image);
     }
 }
-renderOrder();
 /* 
 function storeInLocalStorage(){
     let stringArray=JSON.stringify(banana);
